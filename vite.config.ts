ac567cd5812e9f29517d72e6b8fc1fb994269387
dev-patrick-alunya <patrickalunya106@gmail.com>
2026-05-04 11:53:24 +0300

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,10 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig();
+export default defineConfig({
+  vite: {
+	server: {
+	  allowedHosts: ["https://stellar-bank-system.onrender.com/", "stellar-bank-system"],
+	},
+  },
+});
