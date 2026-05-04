ce76cda27244e0fd2a2c3f5f936dec32b136736a
dev-patrick-alunya <patrickalunya106@gmail.com>
2026-05-04 11:57:54 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
 	server: {
-	  allowedHosts: ["https://stellar-bank-system.onrender.com/", "stellar-bank-system"],
+	  allowedHosts: ["stellar-bank-system.onrender.com"],
 	},
   },
 });
